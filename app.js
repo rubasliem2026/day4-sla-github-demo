0843939be66398d4ad050511c5450215eef1ba21
rubasliem <rubasliem@gmail.com>
2026-05-10 02:41:15 +0300

    add support case starter file

---
+++ app.js
@@ -0,0 +1,9 @@
+const caseRecord = {
+  id: "CASE-101",
+  slaBreached: true,
+  escalated: false
+};
+
+console.log("Case is loaded:", caseRecord.id);
+console.log("SLA breached:", caseRecord.slaBreached);
+console.log("Already escalated:", caseRecord.escalated);
